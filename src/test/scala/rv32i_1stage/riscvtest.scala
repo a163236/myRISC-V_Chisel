017980b70e855a86d3e61da2f0aff36f2d6ac4a1
KeitaN <51395204+a163236@users.noreply.github.com>
2020-09-02 18:04:42 +0900

    dmem

--- src/test/scala/rv32i_1stage/riscvtest.scala
+++ src/test/scala/rv32i_1stage/riscvtest.scala
@@ -34,7 +34,7 @@ class riscvtest extends FlatSpec with ChiselScalatestTester with Matchers{
         c.clock.step(1)
         println()
       }
-      c.io.debug.out.expect(1.U) // gpレジスタが1ならパス
+      c.io.debug.out.expect(0.U) // gpレジスタが1ならパス
     }
   }
 
