096a463786e1b2d6f95a0f0240b73080ba145938
KeitaN <51395204+a163236@users.noreply.github.com>
2020-09-08 15:27:04 +0900

    riscv-tests

--- src/main/scala/rv32i_1stage/Memory.scala
+++ src/main/scala/rv32i_1stage/Memory.scala
@@ -36,7 +36,7 @@ class Memory(implicit val conf:Configurations) extends Module {
     val led = new DataMemoryLEDIO()
   })
   io := DontCare
-  val mem_0,mem_1,mem_2,mem_3 = SyncReadMem(64*1024, UInt(8.W))
+  val mem_0,mem_1,mem_2,mem_3 = Mem(64*1024, UInt(8.W))
   val addr = io.mport.req.bits.addr
   val typ = io.mport.req.bits.typ
 
@@ -50,18 +50,23 @@ class Memory(implicit val conf:Configurations) extends Module {
     tmpaddr(2) := Mux((addr)(1,0)<=2.U, addr(31,2), addr(31,2)+1.U) // mem_2のアドレス
     tmpaddr(3) := Mux((addr)(1,0)<=3.U, addr(31,2), addr(31,2)+1.U) // mem_3のアドレス
 
-    when(typ===MT_B){       wmask(0):=false.B;wmask(1):=false.B;wmask(2):=false.B;wmask(3):=true.B;
-    }.elsewhen(typ===MT_H){ wmask(0):=false.B;wmask(1):=false.B;wmask(2):=true.B;wmask(3):=true.B;
+    when(typ===MT_B){       wmask(0):=true.B;wmask(1):=false.B;wmask(2):=false.B;wmask(3):=false.B;
+    }.elsewhen(typ===MT_H){ wmask(0):=true.B;wmask(1):=true.B;wmask(2):=false.B;wmask(3):=false.B;
     }.otherwise{            wmask(0):=true.B;wmask(1):=true.B;wmask(2):=true.B;wmask(3):=true.B;}
 
     val wdata_addr = Wire(Vec(4, UInt(2.W)))  // 書き込みデータのインターリーブごとのアドレス
     val tmpwdata = Wire(Vec(4, UInt(8.W)))    // 書き込みデータをバイト単位で分割
     wdata_addr(0):=0.U-addr(1,0);wdata_addr(1):=1.U-addr(1,0);wdata_addr(2):=2.U-addr(1,0);wdata_addr(3):=3.U-addr(1,0);
     tmpwdata(0):=data(7,0);tmpwdata(1):=data(15,8);tmpwdata(2):=data(23,16);tmpwdata(3):=data(31,24);
-    when(wmask(0.U+addr(1,0))){mem_0.write(tmpaddr(0),tmpwdata(0.U-addr(1,0)))}
-    when(wmask(1.U+addr(1,0))){mem_1.write(tmpaddr(1),tmpwdata(1.U-addr(1,0)))}
-    when(wmask(2.U+addr(1,0))){mem_2.write(tmpaddr(2),tmpwdata(2.U-addr(1,0)))}
-    when(wmask(3.U+addr(1,0))){mem_3.write(tmpaddr(3),tmpwdata(3.U-addr(1,0)))}
+    when(wmask(0.U-addr(1,0))){mem_0.write(tmpaddr(0),tmpwdata(0.U-addr(1,0)))}
+    when(wmask(1.U-addr(1,0))){mem_1.write(tmpaddr(1),tmpwdata(1.U-addr(1,0)))}
+    when(wmask(2.U-addr(1,0))){mem_2.write(tmpaddr(2),tmpwdata(2.U-addr(1,0)))}
+    when(wmask(3.U-addr(1,0))){mem_3.write(tmpaddr(3),tmpwdata(3.U-addr(1,0)))}
+
+    //printf("%x %x ", tmpaddr(3), tmpwdata(3.U-addr(1,0)))
+    //printf("%x ", tmpwdata(0.U-addr(1,0)))
+    //printf("%x ", (addr(1,0)))
+    //printf("%x %x %x %x ",tmpwdata(3),tmpwdata(2),tmpwdata(1) ,tmpwdata(0))
 
     /*
     for (i <- 0 until bytes){
@@ -74,7 +79,6 @@ class Memory(implicit val conf:Configurations) extends Module {
     }
 
      */
-
   }
 
   def load(): UInt ={     // ===================================== load関数
@@ -87,15 +91,20 @@ class Memory(implicit val conf:Configurations) extends Module {
     tmpaddr(2) := Mux((addr)(1,0)<=2.U, addr(31,2), addr(31,2)+1.U) // mem_2のアドレス
     tmpaddr(3) := Mux((addr)(1,0)<=3.U, addr(31,2), addr(31,2)+1.U) // mem_3のアドレス
     databank(0):=mem_0(tmpaddr(0)); databank(1):=mem_1(tmpaddr(1));databank(2):=mem_2(tmpaddr(2));databank(3):=mem_3(tmpaddr(3))
-    val offset = addr(1,0)
+    val offset = Wire(UInt(2.W)); offset:=addr(1,0)
+
     ret := MuxLookup(io.mport.req.bits.typ, 0.U, Array(
       MT_W -> Cat(databank(offset+3.U),databank(offset+2.U),databank(offset+1.U),databank(offset)),
       MT_WU-> Cat(databank(offset+3.U),databank(offset+2.U),databank(offset+1.U),databank(offset)),
       MT_H -> Cat(Fill(16,databank(offset+1.U)(7)) ,databank(offset+1.U),databank(offset)),
       MT_HU-> Cat(Fill(16,0.U), databank(offset+1.U),databank(offset)),
-      MT_B -> Cat(Fill(24, databank(offset)) ,databank(offset)),
+      MT_B -> Cat(Fill(24, databank(offset)(7)) ,databank(offset)),
       MT_BU-> Cat(Fill(24,0.U), databank(offset)),
     ))
+    //printf("%x ",Cat(Fill(24, databank(offset)(7)) ,databank(1)))
+    //printf("%x %x %x %x ",databank(3),databank(2),databank(1),databank(0))
+
+    //printf("%x ",mem_0("h800".U))
     ret
   }
 
